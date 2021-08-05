bfdf708bd4758b9c9ea3750e57a2bb738e1978d3
Lamonte1066 <mark.lamonte@gmail.com>
2021-08-04 21:52:41 -0500

    Range

---
+++ ruby-fundamentals/range.rb
@@ -0,0 +1,16 @@
+=begin
+.include?(value) => true or false
+.last => returns the last object in range
+.max => returns the maximum value in range
+.min => returns the minimum value in range
+=end
+
+dinos = ["Tyrannosaurus Rex","Triceratops","Stegasaurus","Pternodon"]
+
+puts dinos.include?("birds")
+puts dinos.include?("Triceratops")
+
+puts dinos.last
+puts dinos.max
+
+puts.dinos.min
