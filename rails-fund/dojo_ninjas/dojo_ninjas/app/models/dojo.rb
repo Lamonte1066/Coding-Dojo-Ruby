795522b7bb0d0c96bb369aba8e8bf6f41d8ec7e4
Lamonte1066 <mark.lamonte@gmail.com>
2021-08-08 23:09:37 -0500

    Dojos and Ninjas

---
+++ rails-fund/dojo_ninjas/dojo_ninjas/app/models/dojo.rb
@@ -0,0 +1,10 @@
+class Dojo < ActiveRecord::Base
+    has_many :ninjas
+    validates :name, :city, :state, presence: true
+    validates :state length: {is: 2}
+    after_destroy :delete_ninjas
+
+    private
+    def delete_ninjas
+        self.ninjas.destroy_all
+end
